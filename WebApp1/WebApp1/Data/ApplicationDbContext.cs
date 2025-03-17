f35cf77b6519b33f1fc5d9abe25a4e80c80e9d57
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-17 16:31:41 +0100

    commit

---
+++ WebApp1/WebApp1/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp1.Models;
+
+namespace WebApp1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
