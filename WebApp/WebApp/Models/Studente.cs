13b05116c492e19d88b93dfbf14bc46f21bf6ba1
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-17 15:45:59 +0100

    commit

---
+++ WebApp/WebApp/Models/Studente.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models
+{
+    public class Studente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateOnly DataDiNascita { get; set; }
+        public string Email { get; set; }
+
+    }
+}
